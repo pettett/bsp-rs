5b6c083e5f4cc83801f3ae4b331a774ac9e0094c
Maxwell <43613592+pettett@users.noreply.github.com>
2023-08-26 13:24:24 +0100

    use glam in uniform

--- src/camera.rs
+++ src/camera.rs
@@ -59,18 +59,16 @@ impl Camera {
 // This is so we can store this in a buffer
 #[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct CameraUniform {
-    // We can't use cgmath with bytemuck directly so we'll have
-    // to convert the Matrix4 into a 4x4 f32 array
-    view_proj: [[f32; 4]; 4],
+    view_proj: Mat4,
 }
 impl CameraUniform {
     pub fn new() -> Self {
         Self {
-            view_proj: Mat4::IDENTITY.to_cols_array_2d(),
+            view_proj: Mat4::IDENTITY,
         }
     }
 
     pub fn update_view_proj(&mut self, camera: &Camera) {
-        self.view_proj = camera.build_view_projection_matrix().to_cols_array_2d();
+        self.view_proj = camera.build_view_projection_matrix();
     }
 }
